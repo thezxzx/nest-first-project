36958755a3fa6a6152e3ad3219783fa0aaa3784b
thezxzx <themasterzxzx@gmail.com>
2023-03-07 16:54:11 -0700

    Ejemplo get con segmento dinamico

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,16 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+  private cars = [
+    'Toyota',
+    'Honda',
+    'Jeep',
+  ];
+
   // Petición get
   @Get()
   getAllCars() {
-    return [
-      'Toyota',
-      'Honda',
-      'Jeep',
-    ]
+    return this.cars;
+  }
+
+  // Obtener paŕametro
+  @Get(':id') // Segmento dinámico
+  getCarById( @Param('id') id: any ) { // Obtener el segmento, mismo nombre que el segmento, por defecto va a ser string siempre
+    const car = this.cars[id];
+    if( car ) {
+      console.log({ id });
+      return { car };
+    }
+    return {
+      'Error': `Not found car with id ${id}`
+    }
   }
 
 }
