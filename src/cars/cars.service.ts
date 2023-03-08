08fd4c70593c27414aad10b02383f9c7692f8378
thezxzx <themasterzxzx@gmail.com>
2023-03-07 18:33:39 -0700

    Uso de excepciones de nest en el servicio

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CarsService {
@@ -27,7 +27,13 @@ export class CarsService {
   }
 
   findOneById( id: number ) {
-    return this.cars.find(car => car.id === id);
+
+    const car = this.cars.find(car => car.id === id);
+
+    // Utilizaciones de excepción de nest
+    if( !car ) throw new NotFoundException(`Car with id '${id}' not found`);
+
+    return car;
   }
 
 }
