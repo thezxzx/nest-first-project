8cffd5e42dfe4c508040c7287eccbdbc68ca06bc
thezxzx <themasterzxzx@gmail.com>
2023-03-08 15:02:57 -0700

    Usar pipes globales

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Solo deja la data que se esta esperando
+      forbidNonWhitelisted: true, // Mostrar un error si se envía información que no se espera en el objeto.
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
